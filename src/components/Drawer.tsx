7978b4b547e69f51d1bc404ca830be4954ae4f82
shimotsu4431 <shimotsu13@gmail.com>
2021-01-31 21:14:31 +0900

    fix style

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -35,8 +35,19 @@ const Drawer: React.FC<Props> = ({ openDrawer, setOpenDrawer }) => {
         <div>
           <button className="DrawerCloseButton" onClick={() => setOpenDrawer(false)}>
             <img  className="DrawerCloseButtonImg" src="../close.svg" alt="閉じる"/>
-          </button></div>
+          </button>
         </div>
+        <div className="DrawerInner">
+          <h2 className="DrawerInnerTitle">Drawer Menu</h2>
+          <nav>
+            <ul className="DrawerList">
+              <li>hoge</li>
+              <li>fuga</li>
+              <li>piyo</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
         <div
           ref={overlay}
           onClick={handleClickOverlay}
@@ -44,7 +55,7 @@ const Drawer: React.FC<Props> = ({ openDrawer, setOpenDrawer }) => {
           "overlay": true,
           "isShow": openDrawer
         })}>
-        </div>
+      </div>
     </>
   )
 }

202518c13d959665363c3112b02a22df20bc458b
shimotsu4431 <shimotsu13@gmail.com>
2021-01-31 12:37:49 +0900

    add close button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import clsx from 'clsx';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [openDrawer, setOpenDrawer] = useState(false)
+
   return (
-    <div className="App">
-      <header className="Header">
-        <button className="DrawerButton">
-          <img  className="DrawerButtonImg" src="../button.svg" alt=""/>
-        </button>
-        Header</header>
-    </div>
+    <>
+      <div className="App">
+        <header className="Header">
+          <button className="DrawerButton" onClick={() => setOpenDrawer(true)}>
+            <img  className="DrawerButtonImg" src="../button.svg" alt=""/>
+          </button>
+          Header</header>
+      </div>
+      <div className={clsx({
+        "Drawer": true,
+        "isShow": openDrawer
+      })}>
+      <div>
+        <button className="DrawerCloseButton" onClick={() => setOpenDrawer(false)}>
+          <img  className="DrawerCloseButtonImg" src="../close.svg" alt=""/>
+        </button></div>
+      </div>
+    </>
   );
 }
 
